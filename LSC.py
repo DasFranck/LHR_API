c8762627f29fb74ce99238060aba5bd46dabe4d5
DasFranck <dasfranck@gmx.com>
2016-08-30 11:47:19 +0200

    Changing the main function into a class

--- LSC.py
+++ LSC.py
@@ -7,22 +7,26 @@ import bs4 as BeautifulSoup
 URL = "http://www.rakuten.co.jp/category/fashiongoods/?l-id=top_normal_gmenu_d04"
 
 
-def main():
-    # Get html page
-    html = urllib.request.urlopen(URL).read()
-    # And se BeautifulSoup to parse it
-    soup = BeautifulSoup.BeautifulSoup(html, "html.parser")
+class LHR_API():
+    def __init__(self):
+        self.url_list = []
+        self.result = dict()
 
-    # Extracting tags which contains url to brands page
-    tag_list = soup.find("div", attrs={"class": u"riClfx rigSetHeightWrap riMaB20"})
-    tag_list = tag_list.findAll("p", attrs={"class": "riTxtAlnC"})
+    def start(self):
+        # Get html page
+        html = urllib.request.urlopen(URL).read()
+        # And se BeautifulSoup to parse it
+        soup = BeautifulSoup.BeautifulSoup(html, "html.parser")
 
-    # Extracting url to brands page from tag list
-    url_list = []
-    for element in tag_list:
-        url_list.append(element.find('a', href=True)['href'])
-    print(url_list)
+        # Extracting tags which contains url to brands page
+        tag_list = soup.find("div", attrs={"class": u"riClfx rigSetHeightWrap riMaB20"})
+        tag_list = tag_list.findAll("p", attrs={"class": "riTxtAlnC"})
 
+        # Extracting url to brands page from tag list
+        for element in tag_list:
+            self.url_list.append(element.find('a', href=True)['href'])
 
 if __name__ == "__main__":
-    main()
+    lhr = LHR_API()
+    lhr.start()
+    print(lhr.url_list)
